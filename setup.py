6efa8a8d9da4358e5a5dc69d0fecbbf408b915ae
Maarten van Gompel <proycon@anaproy.nl>
2023-02-22 13:30:00 +0100

    version bump and dependency version bump, for compilation against ucto v0.28

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def getreadme():
 
 setup(
     name = "Ucto Webservice",
-    version = "2.3", #make sure SYSTEM_VERSION in your service configuration is set to the same value!
+    version = "2.4", #make sure SYSTEM_VERSION in your service configuration is set to the same value!
     author = "Maarten van Gompel", #adapt this
     description = ("Ucto is a rule-based tokeniser for multiple languages. This is the webservice for it, for both humans and machines."),
     license = "GPLv3",
@@ -39,5 +39,5 @@ setup(
     ],
     package_data = {'ucto_webservice':['*.wsgi','*.yml','*.sh'] },
     include_package_data=True,
-    install_requires=['CLAM >= 3.1.4', 'FoLiA-tools'] #Ucto is also required but is an external dependency that setuptools can't handle, we specify it in codemeta-harvest.json for metadata harvesting purposes
+    install_requires=['CLAM >= 3.2.4', 'FoLiA-tools'] #Ucto is also required but is an external dependency that setuptools can't handle, we specify it in codemeta-harvest.json for metadata harvesting purposes
 )
--- ucto_webservice/ucto_webservice.py
+++ ucto_webservice/ucto_webservice.py
@@ -26,12 +26,13 @@ import ucto_webservice
 import os
 from base64 import b64decode as D
 
-REQUIRE_VERSION = "3.1.4"
+REQUIRE_VERSION = "3.2.4"
 WRAPPERDIR = ucto_webservice.__path__[0]
 
 SYSTEM_ID = "ucto"
 SYSTEM_NAME = "Ucto Webservice"
 SYSTEM_DESCRIPTION = 'Ucto is a unicode-compliant tokeniser. It takes input in the form of one or more untokenised texts, and subsequently tokenises them. Several languages are supported, but the software is extensible to other languages.'
+SYSTEM_VERISON = "2.4"
 
 SYSTEM_AUTHOR = "Maarten van Gompel, Ko van der Sloot"
 
